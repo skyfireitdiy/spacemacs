d39914d1220aef2f2f1d626e5c0fb5954149cdd9
skyfire <skyfireitdiy@hotmail.com>
2019-09-24 22:43:39 +0800

    删除clang自动格式化

--- init.el
+++ init.el
@@ -40,8 +40,6 @@ values."
      d
      (c-c++ :variables
             c-c++-default-mode-for-headers 'c++-mode
-            c-c++-enable-clang-support t
-
             )
 
      helm
@@ -536,13 +534,6 @@ you should place your code here."
   ;; ---------------------------------------------------------------------------------------
   ;; 快捷键配置
 
-
-  ;; Bind clang-format-region to C-M-tab in all modes:
-  (global-set-key [C-M-tab] 'clang-format-region)
-  ;; Bind clang-format-buffer to tab on the c++-mode only:
-  (add-hook 'c++-mode-hook 'clang-format-bindings)
-  (defun clang-format-bindings ()
-    (define-key c++-mode-map [tab] 'clang-format-buffer))
 
   ;; enable this if you want `swiper' to use it
   ;; (setq search-default-mode #'char-fold-to-regexp)
